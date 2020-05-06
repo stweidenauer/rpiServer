d5d9295434089bec3a4804916425f794742dca6e
Steffen <stweidenauer@gmail.com>
2020-05-06 12:31:53 +0000

    Route camera angelegt

--- run.py
+++ run.py
@@ -8,5 +8,11 @@ def index():
     return render_template('Index.html')
 
 
+@app.route('/camera')
+def camera():
+    pictures = ['Bild1', 'Bild2']
+    return render_template('camera.html', pictures=pictures)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
